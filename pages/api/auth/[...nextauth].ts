666aa2a3ed5e732459903df493a28ba401c747d3
wwsalmon <wwsamson@yahoo.com>
2021-01-29 10:37:31 -0700

    add nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,35 @@
-import NextAuth from "next-auth";
+import NextAuth, {InitOptions} from "next-auth";
 import Providers from "next-auth/providers";
+import {NextApiRequest, NextApiResponse} from "next";
+import mongoose from "mongoose";
+import {UserModel} from "../../../models/user";
 
-const options = {
+const options: InitOptions = {
     providers: [
         Providers.Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         }),
     ],
+    callbacks: {
+        jwt: async (token, user, account, profile, isNewUser) => {
+            if (user) {
+                await mongoose.connect(process.env.MONGODB_URL, {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true,
+                    useFindAndModify: false,
+                });
+
+                const foundUser = await UserModel.findOne({ email: user.email }).exec();
+
+                if (foundUser) {
+                    token.userId = foundUser._id;
+                }
+            }
+
+            return token;
+        }
+    }
 };
 
-export default (req, res) => NextAuth(req, res, options);
+export default (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, options);
