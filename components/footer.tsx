7e73660671d89aab1bf59f1ea5967809dcfada25
Samson Zhang <wwsamson@yahoo.com>
2021-02-28 09:43:22 -0700

    clean up navbar, footer, inline ctas

---
+++ components/footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <>
+            <div className="max-w-7xl mx-auto px-4 opacity-25 py-6 mt-8">
+                <p>
+                    Follow <a href="https://twitter.com/postulateapp" className="underline">Postulate on Twitter</a>.
+                    Built with ♥ by <a href="https://twitter.com/wwsalmon" className="underline">Samson Zhang</a>
+                </p>
+            </div>
+        </>
+    );
+}
