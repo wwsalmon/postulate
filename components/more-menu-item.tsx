2cb97bcc8fc653dc833fce92e1e0bdd176239ba7
Samson Zhang <wwsamson@yahoo.com>
2021-03-03 00:57:56 -0700

    add projects and profile links to navbar

--- components/more-menu-item.tsx
+++ components/more-menu-item.tsx
@@ -1,26 +1,27 @@
 import {ReactNode} from "react";
 import Link from "next/link";
 
-export default function MoreMenuItem({href, onClick, disabled = false, icon = null, text}: {
+export default function MoreMenuItem({href, onClick, disabled = false, icon = null, text, className = ""}: {
     href?: string,
     onClick?: () => any,
     disabled?: boolean,
     icon?: ReactNode,
     text: string,
+    className?: string,
 }) {
     const MoreMenuItemClasses = "h-12 flex items-center hover:bg-gray-50 px-4 w-full";
     const MoreMenuItemDisabledClasses = " opacity-50 cursor-not-allowed hover:bg-white"
 
     return href ? (
         <Link href={href}>
-            <a className={MoreMenuItemClasses + (disabled ? MoreMenuItemDisabledClasses : "")}>
+            <a className={MoreMenuItemClasses + (disabled ? MoreMenuItemDisabledClasses : "") + " " + className}>
                 {icon}
                 <span className={"whitespace-nowrap " + (icon ? "ml-4" : "")}>{text}</span>
             </a>
         </Link>
     ) : (
         <button
-            className={MoreMenuItemClasses + (disabled ? MoreMenuItemDisabledClasses : "")}
+            className={MoreMenuItemClasses + (disabled ? MoreMenuItemDisabledClasses : "") + " " + className}
             disabled={disabled}
             onClick={onClick}
         >
