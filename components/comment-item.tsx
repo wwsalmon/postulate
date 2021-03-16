908ed1fcc61e23d58aacb55b9c6c76d1a3798a86
Samson Zhang <wwsamson@yahoo.com>
2021-03-15 21:23:53 -0600

    edit and delete comment functionality

--- components/comment-item.tsx
+++ components/comment-item.tsx
@@ -1,6 +1,6 @@
 import {CommentObj, CommentWithAuthor, DatedObj, UserObj} from "../utils/types";
 import Link from "next/link";
-import {format} from "date-fns";
+import {format, formatDistanceToNow} from "date-fns";
 import {Dispatch, SetStateAction, useState} from "react";
 import {responseInterface} from "swr";
 import useSWR from "swr";
@@ -8,6 +8,9 @@ import {fetcher} from "../utils/utils";
 import axios from "axios";
 import SpinnerButton from "./spinner-button";
 import Linkify from "react-linkify";
+import {FiCornerDownRight} from "react-icons/all";
+import {useSession} from "next-auth/client";
+import UpModal from "./up-modal";
 
 interface PropsBase {
     iteration: number,
@@ -29,20 +32,26 @@ interface PropsExisting extends PropsBase {
 }
 
 export default function CommentItem({ authorId, comment, targetId, parentCommentId, iteration, setIteration }: PropsNew | PropsExisting) {
+    const [session, loading] = useSession();
     const [body, setBody] = useState<string>(comment ? comment.body : "");
     const [commentLoading, setCommentLoading] = useState<boolean>(false);
+    const [deleteOpen, setDeleteOpen] = useState<boolean>(false);
+    const [deleteLoading, setDeleteLoading] = useState<boolean>(false);
+    const [isEdit, setIsEdit] = useState<boolean>(false);
 
     const {data: author, error: authorError}: responseInterface<{ data: DatedObj<UserObj> }, any> = useSWR(`/api/user?id=${authorId || ""}`, authorId ? fetcher : () => null)
 
     const authorObj = authorId ? ((author && author.data) ? author.data : {name: "", image: "", username: ""}) : comment.author[0];
     const disablePost = !body || (comment ? body === comment.body : false);
+    const isAuthor = session && comment && session.userId === comment.userId;
 
-    function PostComment() {
+    function onSubmit() {
         const postData = authorId ? {
             targetId: targetId,
             parentCommentId: parentCommentId,
             body: body,
         } : {
+            id: comment._id,
             body: body,
         }
 
@@ -58,25 +67,62 @@ export default function CommentItem({ authorId, comment, targetId, parentComment
         });
     }
 
+    function onDelete() {
+        setDeleteLoading(true);
+
+        axios.delete("/api/comment", { data: { id: comment ? comment._id : "" } }).then(() => {
+            setIteration(iteration + 1);
+        }).catch(e => {
+            console.log(e);
+            setDeleteLoading(false);
+        })
+    }
+
     return (
         <div className="flex w-full mb-8">
             <Link href={`/@${authorObj.username}`}>
                 <a>
-                    <img src={authorObj.image} alt={`Profile picture of ${authorObj.name}`} className={"w-10 rounded-full mr-4" + (authorId ? " mt-2 opacity-25" : "")}/>
+                    <img src={authorObj.image} alt={`Profile picture of ${authorObj.name}`} className={"w-10 rounded-full mr-4 mt-2" + (authorId ? " opacity-25" : "")}/>
                 </a>
             </Link>
             <div className="w-full">
-                {comment ? (
+                {(comment && !isEdit) ? (
                     <>
                         <div className="flex items-center">
                             <Link href={`/@${authorObj.username}`}>
                                 <a className="font-bold inline-block mr-3">{authorObj.name}</a>
                             </Link>
-                            <span className="opacity-25">{format(new Date(comment.createdAt), "M/d/yyyy 'at' h:mm a")}</span>
+                            <span className="opacity-25">{formatDistanceToNow(new Date(comment.createdAt))} ago</span>
                         </div>
-                        <p className="mt-2 prose">
+                        <p className="mt-1 prose">
                             <Linkify>{comment.body}</Linkify>
                         </p>
+                        <div className="mt-2 flex items-center">
+                            <button className="mr-4 opacity-25 hover:opacity-50 transition">
+                                Reply
+                            </button>
+                            {isAuthor && (
+                                <>
+                                    <button className="mr-4 opacity-25 hover:opacity-50 transition" onClick={() => setIsEdit(true)}>
+                                        Edit
+                                    </button>
+                                    <button className="mr-4 opacity-25 hover:opacity-50 transition" onClick={() => setDeleteOpen(true)}>
+                                        Delete
+                                    </button>
+                                    <UpModal isOpen={deleteOpen} setIsOpen={setDeleteOpen}>
+                                        Are you sure you want to delete this comment?
+                                        <div className="mt-4 flex">
+                                            <SpinnerButton isLoading={deleteLoading} onClick={onDelete}>
+                                                Delete
+                                            </SpinnerButton>
+                                            <button className="up-button text" onClick={() => setDeleteOpen(false)}>
+                                                Cancel
+                                            </button>
+                                        </div>
+                                    </UpModal>
+                                </>
+                            )}
+                        </div>
                     </>
                 ) : (
                     <>
@@ -88,14 +134,22 @@ export default function CommentItem({ authorId, comment, targetId, parentComment
                         />
                         <div className="flex w-full justify-end mb-4">
                             <SpinnerButton
-                                onClick={PostComment}
+                                onClick={onSubmit}
                                 isLoading={commentLoading}
                                 isDisabled={disablePost}
-                                noRightMargin
+                                noRightMargin={!isEdit}
                                 className="small"
                             >
-                                Post
+                                {isEdit ? "Save" : "Post"}
                             </SpinnerButton>
+                            {isEdit && (
+                                <button className="up-button small text" onClick={() => {
+                                    setBody(comment.body);
+                                    setIsEdit(false);
+                                }}>
+                                    Cancel
+                                </button>
+                            )}
                         </div>
                     </>
                 )}
--- pages/api/comment.ts
+++ pages/api/comment.ts
@@ -1,12 +1,11 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import dbConnect from "../../utils/dbConnect";
 import {getSession} from "next-auth/client";
-import {ReactionModel} from "../../models/reaction";
 import {CommentModel} from "../../models/comment";
 import * as mongoose from "mongoose";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== "POST" && req.method !== "GET") return res.status(405).json({message: "Invalid method"});
+    if (req.method !== "POST" && req.method !== "GET" && req.method !== "DELETE") return res.status(405).json({message: "Invalid method"});
 
     let session;
     if (req.method === "POST") {
@@ -14,8 +13,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         if (!session || !session.userId) return res.status(403).json({message: "Not authed"});
         if (!req.body.targetId && !req.body.id) return res.status(406).json({message: "Missing target or comment ID"});
         if (!req.body.body) return res.status(406).json({message: "Missing comment body"});
-    } else {
+    } else if (req.method === "GET") {
         if (!req.query.targetId) return res.status(406).json({message: "Missing target ID"});
+    } else if (req.method === "DELETE") {
+        if (!req.body.id) return res.status(406).json({message: "Missing comment ID"});
     }
 
     try {
@@ -24,8 +25,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         if (req.method === "POST") {
             // if has id body param, update existing comment, else create new
             if (req.body.id) {
-                const existingComment = await ReactionModel.findOne({ _id: req.body.id });
+                const existingComment = await CommentModel.findById(req.body.id);
                 if (!existingComment) return res.status(404).json({message: "No comment found"});
+                if (existingComment.userId !== session.userId) return res.status(403).json({message: "unauthed"});
                 existingComment.body = req.body.body;
                 await existingComment.save();
                 return res.status(200).json({message: "Comment updated"});
@@ -39,9 +41,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
                 return res.status(200).json({message: "Comment created"});
             }
-
-            return res.status(200).json({message: "Comment updated"});
-        } else {
+        } else if (req.method === "GET") {
             if (Array.isArray(req.query.targetId)) return res.status(406).json({message: "Invalid target ID"});
             const comments = await CommentModel.aggregate([
                 {
@@ -64,6 +64,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             ]);
 
             return res.status(200).json({data: comments});
+        } else if (req.method === "DELETE") {
+            await CommentModel.deleteOne({ _id: req.body.id });
+
+            return res.status(200).json({message: "Comment deleted"});
         }
     } catch (e) {
         return res.status(500).json({message: e});
