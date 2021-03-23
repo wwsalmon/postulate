fd463750dc0b76b549c0fe2a9a6fc1c4870a3957
Samson Zhang <wwsamson@yahoo.com>
2021-03-23 12:50:51 -0600

    fix PS-106

--- components/up-modal.tsx
+++ components/up-modal.tsx
@@ -7,14 +7,14 @@ export default function UpModal({isOpen, setIsOpen, children, wide = false}: {
     children: ReactNode,
     wide?: boolean,
 }) {
-    const ModalClasses = "top-24 left-1/2 fixed bg-white p-4 rounded-md shadow-xl" + (wide ? " max-w-2xl" : " max-w-sm");
+    const ModalClasses = "top-24 left-1/2 fixed bg-white p-4 rounded-md shadow-xl mx-4";
 
     return (
         <Modal
             isOpen={isOpen}
             onRequestClose={() => setIsOpen(false)}
             className={ModalClasses}
-            style={{content: {transform: "translateX(-50%)"}, overlay: {zIndex: 20}}}
+            style={{content: {transform: "translateX(calc(-50% - 16px))", maxWidth: "calc(100% - 32px)", width: wide ? 700 : 320}, overlay: {zIndex: 20}}}
         >
             {children}
         </Modal>
