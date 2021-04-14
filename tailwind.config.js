5d4fa2d0796d01b078463badf2e87076fdc39030
Samson Zhang <wwsamson@yahoo.com>
2021-04-14 17:50:16 -0400

    update tailwind prose styling for md-to-slate conversion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 let t = {};
-t["p"] = t["img"] = t["video"] = t["figure"] = t["ol"] = t["ul"] = {
+t["p"] = t["img"] = t["video"] = t["figure"] = {
     paddingTop: "0.75em",
     paddingBottom: "0.75em",
     marginTop: 0,
     marginBottom: 0,
 }
+t["ol"] = t["ul"] = {
+    paddingTop: 0,
+    paddingBottom: "0.75em",
+    marginTop: 0,
+    marginBottom: 0,
+}
 t["h1"] = t["h2"] = t["h3"] = t["h4"] = t["h5"] = t["h6"] = {
-    paddingTop: "2em",
+    paddingTop: "1.5em",
     paddingBottom: "0.75em",
     marginTop: 0,
     marginBottom: 0,
