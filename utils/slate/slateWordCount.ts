c89e6e7354168cea527aaadf898762254cebe30f
Samson Zhang <wwsamson@yahoo.com>
2021-07-21 16:24:57 -0700

    add word count to slate editor

---
+++ utils/slate/slateWordCount.ts
@@ -0,0 +1,12 @@
+import {Node} from "slate";
+
+export default function slateWordCount(body: Node[]) {
+    let length = 0;
+    for (let node of body) {
+        // @ts-ignore
+        if (node.text) length += node.text.split(/\b\W+\b/).length;
+        // @ts-ignore
+        else length += slateWordCount(node.children);
+    }
+    return length;
+}
