127fcf08be5f3fc25dd5224609fd900e07f34878
Samson Zhang <wwsamson@yahoo.com>
2022-01-16 02:56:01 -0500

    implement p/url for shortcuts

--- pages/[username]/[projectUrlName]/p/[urlName].tsx
+++ pages/[username]/[projectUrlName]/p/[urlName].tsx
@@ -11,15 +11,27 @@ import PublicNavbar from "../../../../components/project/PublicNavbar";
 import UserButton from "../../../../components/standard/UserButton";
 import React from "react";
 import getPublicNodeSSRFunction, {PublicNodePageProps} from "../../../../utils/getPublicNodeSSRFunction";
+import InlineButton from "../../../../components/style/InlineButton";
+import {FiExternalLink} from "react-icons/fi";
 
 export default function PublicPostPage({pageUser, pageProject, pageNode, thisUser}: PublicNodePageProps) {
     const {body: {publishedTitle: title, publishedBody: body, publishedDate, lastPublishedDate}} = pageNode;
     const isOwner = thisUser && pageNode.userId === thisUser._id;
+    const isExternal = !!pageNode.shortcutArr;
+    const originalProject = isExternal && pageNode.orrProjectArr[0];
 
     return (
         <Container>
             <SEO title={title || `Untitled post`}/>
             <div className="pt-8 pb-32 mx-auto" style={{maxWidth: "78ch"}}> {/* 78ch bc font size is 16 here but we want 65ch for font size 20 */}
+                {isExternal && (
+                    <div className="flex items-center mb-8">
+                        <FiExternalLink/>
+                        <InlineButton href={`${getProjectUrl(pageUser, originalProject)}/p/${pageNode.body.urlName}`} className="ml-2">Originally published</InlineButton>
+                        <span className="mx-4"> in </span>
+                        <InlineButton href={getProjectUrl(pageUser, originalProject)}>{originalProject.name}</InlineButton>
+                    </div>
+                )}
                 <H1>{title}</H1>
                 <UserButton user={pageUser} className="mt-8"/>
                 <div className="flex items-center mt-2 mb-8 font-manrope text-gray-400 font-semibold">
