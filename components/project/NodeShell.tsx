0af56b80b9768c0ccdead19a71d01e48b6858c11
Samson Zhang <wwsamson@yahoo.com>
2022-01-16 20:17:22 -0500

    fix margins on NodeShell

--- components/project/NodeShell.tsx
+++ components/project/NodeShell.tsx
@@ -5,6 +5,8 @@ import NodeInner from "./NodeInner";
 import {Tab} from "@headlessui/react";
 import {Fragment} from "react";
 import NodeFeed from "./NodeFeed";
+import getProjectUrl from "../../utils/getProjectUrl";
+import Link from "next/link";
 
 export default function NodeShell(props: PublicNodePageProps) {
     const {pageNode, pageUser, pageProject, thisUser} = props;
@@ -19,15 +21,19 @@ export default function NodeShell(props: PublicNodePageProps) {
             <PublicNavbar pageUser={pageUser} pageProject={pageProject}/>
             <div className="px-4 flex justify-center">
                 <div
-                    className={`order-1 ${isPost ? "pt-8 pb-32" : isSource ? "my-8" : "p-8 border border-gray-300 rounded-md my-8"}`}
+                    className={`order-1 ${isPost ? "pb-32" : isSource ? "" : "p-8 border border-gray-300 rounded-md"}`}
                     style={{maxWidth: "78ch"}} // 78ch bc font size is 16 here but we want 65ch for font size 20
                 >
                     <NodeInner {...props}/>
                 </div>
                 <div className="w-64 flex-shrink-0 order-0 mr-12">
                     {/*<UserButton user={pageUser}/>*/}
-                    <h2 className="font-manrope font-bold my-4">{pageProject.name}</h2>
-                    <p className="text-gray-400 text-sm my-4">{pageProject.description}</p>
+                    <Link href={getProjectUrl(pageUser, pageProject)}>
+                        <a>
+                            <h2 className="font-manrope font-bold mb-2">{pageProject.name}</h2>
+                        </a>
+                    </Link>
+                    <p className="text-gray-400 text-sm my-2">{pageProject.description}</p>
                     <Tab.Group>
                         <Tab.List className="my-8">
                             {["Posts", "Evergreens", "Sources"].map(tab => (
