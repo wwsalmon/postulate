58ec7d56d3b7973c565342d4a7a4a8da33da8c72
Samson Zhang <wwsamson@yahoo.com>
2022-02-12 11:34:54 -0800

    wip explore feed implementation

---
+++ pages/api/activity.ts
@@ -0,0 +1,27 @@
+import {NextApiHandler} from "next";
+import nextApiEndpoint from "../../utils/nextApiEndpoint";
+import {NodeModel} from "../../models/node";
+import getLookup from "../../utils/getLookup";
+import {res200} from "next-response-helpers";
+
+const handler: NextApiHandler = nextApiEndpoint({
+    getFunction: async (req, res, session, thisUser) => {
+        const {userId, page} = req.query;
+
+        const activity = await NodeModel.aggregate([
+            {
+                $match: {
+                    "body.publishedDate": {$exists: true},
+                }
+            },
+            {$sort: {"createdAt": -1}},
+            {$limit: 30},
+            getLookup("users", "_id", "userId", "userArr"),
+            getLookup("projects", "_id", "projectId", "projectArr"),
+        ]);
+
+        return res200(res, {activity});
+    },
+});
+
+export default handler;
--- pages/explore.tsx
+++ pages/explore.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import SEO from "../components/standard/SEO";
 import H1 from "../components/style/H1";
 import UserSearch from "../components/UserSearch";
+import useSWR from "swr";
+import {fetcher} from "../utils/utils";
+import {DatedObj, NodeObj, ProjectObj, UserObj} from "../utils/types";
+import UserButton from "../components/standard/UserButton";
+import PostItem from "../components/project/PostItem";
+import NodeCard from "../components/project/NodeCard";
+import InlineButton from "../components/style/InlineButton";
+import getProjectUrl from "../utils/getProjectUrl";
 
 export default function OldExplore({}: {  }) {
+    const {data, error} = useSWR<{activity: (DatedObj<NodeObj> & {userArr: DatedObj<UserObj>[], projectArr: DatedObj<ProjectObj>[]})[]}>("/api/activity", fetcher);
+
+    console.log(data);
 
     return (
         <div className="max-w-5xl mx-auto px-4">
@@ -13,6 +24,31 @@ export default function OldExplore({}: {  }) {
             <UserSearch/>
             <hr className="my-8"/>
             <p>A full explore page is coming soon! <a href="https://twitter.com/postulateapp" className="underline">Follow Postulate on Twitter</a> for the latest updates.</p>
+            {data && data.activity && data.activity.map(item => (
+                <div key={item._id} className="my-16">
+                    <div className="flex items-center my-4">
+                        <UserButton user={item.userArr[0]}/>
+                        <span className="mx-2"> published a{item.type === "evergreen" ? "n" : ""} {item.type} in</span>
+                        <InlineButton href={getProjectUrl(item.userArr[0], item.projectArr[0])}>{item.projectArr[0].name}</InlineButton>
+                    </div>
+                    {item.type === "post" && (
+                        <PostItem
+                            pageUser={item.userArr[0]}
+                            pageProject={item.projectArr[0]}
+                            thisUser={null}
+                            pageNode={item}
+                        />
+                    )}
+                    {(item.type === "source" || item.type === "evergreen") && (
+                        <NodeCard
+                            pageUser={item.userArr[0]}
+                            pageProject={item.projectArr[0]}
+                            thisUser={null}
+                            pageNode={item}
+                        />
+                    )}
+                </div>
+            ))}
         </div>
     )
 }
