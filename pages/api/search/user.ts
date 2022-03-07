cd4ecaf502470ceb6e73c5cff0dd61d841889102
Samson Zhang <wwsamson@yahoo.com>
2022-03-06 23:37:20 -0800

    add user search on explore page (close #77)

--- pages/api/search/user.ts
+++ pages/api/search/user.ts
@@ -5,14 +5,12 @@ import dbConnect from "../../../utils/dbConnect";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== "GET") return res.status(405);
 
-    if (!req.query.email && !req.query.query) return res.status(406).json({message: "No email or query found in request"});
-
-    if (Array.isArray(req.query.query)) return res.status(406).json({message: "Invalid query"});
+    if (!req.query.query) return res.status(406).json({message: "No query found in request"});
 
     try {
         await dbConnect();
 
-        const matchUsers = await UserModel.find(req.query.email ? {"email": {$regex: `.*${req.query.email}.*`, $options: "i"}} : {$text: {$search: req.query.query}});
+        const matchUsers = await UserModel.find({"name": {$regex: `.*${req.query.query}.*`, $options: "i"}}).limit(10);
 
         res.status(200).json({results: matchUsers});
 
--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,14 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 import SEO from "../components/standard/SEO";
 import H1 from "../components/style/H1";
 import ExploreFeed from "../components/explore/ExploreFeed";
+import useSWR from "swr";
+import {DatedObj, UserObj} from "../utils/types";
+import {fetcher} from "../utils/utils";
+import UserButton from "../components/standard/UserButton";
+import {FiSearch} from "react-icons/fi";
+import {getInputStateProps} from "react-controlled-component-helpers";
 
-export default function OldExplore({}: {  }) {
+function ExploreUserSearch() {
+    const [query, setQuery] = useState<string>("");
+
+    const {data} = useSWR<{results: DatedObj<UserObj>[]}>(`/api/search/user?query=${query}`, query ? fetcher : async () => []);
+
+    return (
+        <>
+            <div className="md:flex items-center my-4">
+                <p>See what fellow Postulate users are learning, reading and writing.</p>
+                <div className="flex items-center ml-auto my-6 md:my-0">
+                    <FiSearch className="mr-2 text-gray-400"/>
+                    <input
+                        type="text"
+                        placeholder="Search users"
+                        className="w-24 focus:outline-none" {...getInputStateProps(query, setQuery)}
+                    />
+                </div>
+            </div>
+            {data && data.results && data.results.length ? (
+                <>
+                    {data.results.map(user => (
+                        <UserButton user={user} key={user._id} className="mr-4"/>
+                    ))}
+                </>
+            ) : (query && (
+                <p className="up-gray-400">No results found</p>
+            ))}
+        </>
+    )
+}
+
+export default function Explore({}: {  }) {
     return (
         <>
             <SEO title="Explore"/>
             <div className="max-w-3xl mx-auto px-4 pb-8">
                 <H1>Explore</H1>
+                <ExploreUserSearch/>
             </div>
             <div className="w-full bg-gray-100 py-8 border-t">
                 <div className="max-w-3xl mx-auto px-4">
