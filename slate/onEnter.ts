6a3abc51613139845a3efa811781f57d9afda19a
Samson Zhang <wwsamson@yahoo.com>
2022-09-28 14:17:03 -0700

    fix #93 line breaks

--- slate/onEnter.ts
+++ slate/onEnter.ts
@@ -1,7 +1,7 @@
 import {KeyboardEvent} from "react";
 import {ReactEditor} from "slate-react";
 import {HistoryEditor} from "slate-history";
-import {Transforms, Node, Text} from "slate";
+import {Transforms, Node, Text, Editor} from "slate";
 import {onEnterList} from "./list";
 import insertEmptyLine from "./insertEmptyLine";
 
@@ -15,8 +15,15 @@ export const onEnter = (e: KeyboardEvent<HTMLDivElement>, editor: ReactEditor & 
 
         const selectedLeaf = Node.descendant(editor, editor.selection.anchor.path);
 
-        if (Text.isText(selectedLeaf) && selectedLeaf.text.length === editor.selection.anchor.offset) {
+        if (Text.isText(selectedLeaf) && [selectedLeaf.text.length, 0].includes(editor.selection.anchor.offset)) {
             insertEmptyLine(editor);
+
+            if (editor.selection.anchor.offset === 0) {
+                Transforms.select(editor, {
+                    path: [editor.selection.anchor.path[0] + 1, editor.selection.anchor.path[1]],
+                    offset: 0,
+                });
+            }
         } else {
             Transforms.splitNodes(editor);
             Transforms.setNodes(editor, {type: "p"});
